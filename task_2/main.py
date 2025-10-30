c2ad553f557488ecb50f66e9469244c9112ab8b2
arseniimgkct <mgke89172@gmail.com>
2025-10-30 18:16:34 +0300

    done

--- task_2/main.py
+++ task_2/main.py
@@ -8,18 +8,20 @@ qualification = input("Введите номер квалификации: ")
 with open("dump.json", encoding="utf-16") as file:
     data = json.load(file)
 
+found = False
 for item in data:
     model = item.get("model")
     if model in ("data.skill", "data.specialty"):
         fields = item.get("fields", {})
-        code = fields.get("code")
+        code =  fields.get("code")
 
         if code == qualification:
             print(' Найдено '.center(40,"="))
-            print(f'{code} >> Специальность {fields.get("title")}, {fields.get("c_type")} ')
+            ctype = ', ' + fields.get("c_type") if fields.get("c_type") != None else ""
+            print(f'{code} >> Специальность {fields.get("title")}{ctype} ')
             found = True
 
 if not found:
-    print("Не найдено")
+    print("Не найдено".center(40, "="))
 
 print("end code ...")
