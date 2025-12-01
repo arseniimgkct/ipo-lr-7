16d146e9923f294223a1aa366845b62cdd97c762
arseniimgkct <mgkct89172@gmail.com>
2025-12-01 09:05:13 +0300

    Task 6 done

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from collision import isCorrectRect, isCollisionRect, intersectionAreaRect, intersectionAreaMultiRect
 
+# пример использования isCorrectRect
 print(isCorrectRect([(-3.4, 1), (9.2, 10)]))
 print(isCorrectRect([(-7,9), (3,6)]))
 
+
+# пример использования isCorrectRect
 print(isCollisionRect([(-3.4, 1),(9.2, 10)], [(-7.4, 0),(13.2, 12)]))
 print(isCollisionRect([(1, 1),(2, 2)], [(3, 0),(13, 1)]))
 # print(isCollisionRect([(1, 1),(2, 2)], [(3, 17),(13, 1)])) вернет ошибку
 
+# пример использования intersectionAreaRect
 print(intersectionAreaRect([(-3, 1), (9, 10)], [(-7, 0), (13, 12)]))
 print(intersectionAreaRect([(1, 1), (2, 2)], [(3, 0), (13, 1)]))
 # print(intersectionAreaRect([(1, 1), (2, 2)], [(3, 17), (13, 1)])) вернет ошибку
 
+
+# пример использования intersectionAreaMultiRect
 rectangles = [
     [(-3, 1), (9, 10)],
     [(-7, 0), (13, 12)],
